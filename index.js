69b7d3b0c46fb742ab0d61d8ce78c6f3c5bc1298
Alhadis <gardnerjohng@gmail.com>
2022-02-26 09:27:14 +1100

    Add setting to match GitHub's font styling exactly

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 "use strict";
 
+const {name: pkgName} = require("./package.json");
 const {CompositeDisposable} = require("atom");
 const root = document.documentElement.dataset;
+const classes = document.documentElement.classList;
 const inDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 let disposables = null;
 
@@ -9,7 +11,14 @@ module.exports = {
 	activate(){
 		disposables?.dispose();
 		disposables = new CompositeDisposable(
-			atom.config.observe("atom-github-syntax.theme", value => {
+			atom.config.observe(`${pkgName}.matchFont`, value => {
+				if(value !== classes.contains("match-github-font")){
+					classes.toggle("match-github-font");
+					for(const editor of atom.textEditors.editors)
+						editor.component.didUpdateStyles();
+				}
+			}),
+			atom.config.observe(`${pkgName}.theme`, value => {
 				switch(value){
 					case "high-contrast":
 					case "colour-blind":
@@ -26,7 +35,7 @@ module.exports = {
 			}),
 		);
 		inDarkMode.onchange = ({matches}) => {
-			const theme = atom.config.get("atom-github-syntax.theme");
+			const theme = atom.config.get(`${pkgName}.theme`);
 			if("dimmed" === theme)
 				matches ? root.githubTheme = theme : delete root.githubTheme;
 		};
@@ -34,6 +43,8 @@ module.exports = {
 	
 	deactivate(){
 		inDarkMode.onchange = null;
+		classes.remove("match-github-font");
+		delete root.githubTheme;
 		disposables.dispose();
 	},
 };
